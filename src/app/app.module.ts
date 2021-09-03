cf2965793c178d0a9cc8485abd808643ef7addac
Viniboneto <vinicius.b.o.neto@gmail.com>
2021-09-02 22:31:57 -0300

    Fullstack Angular e Spring - Parte de front-end (UI): 10.7. Fazendo paginacao de dados; 10.8. Adicionando tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { AppComponent } from './app.component';
     // TabViewModule
     InputTextModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
